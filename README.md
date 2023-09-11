359d9773f5afbfc31d540dcb465381909e4d47a7
Daniel Ibarrola <dis96051@gmail.com>
2023-09-10 20:29:39 -0600

    Earthworm server program

---
+++ README.md
@@ -0,0 +1,13 @@
+# EarthwormServer
+
+A server that receives data from earthworm and other clients can connect
+to, to receive the data.
+
+## Usage
+
+Download the code and run it in docker:
+
+```shell
+git clone
+docker compose up -d
+```
---
+++ main.py
@@ -0,0 +1,49 @@
+""" Server that received data from earthworm and forwards it to another client
+"""
+import os
+from socketlib import ServerReceiver, ServerSender, WatchDog, get_module_logger
+
+
+def main():
+    logger = get_module_logger("EWServer", "dev", use_file_handler=False)
+
+    receive_address = os.environ["RECEIVE_IP"], int(os.environ["RECEIVE_PORT"])
+    send_address = os.environ["SEND_IP"], int(os.environ["SEND_PORT"])
+    receiver = ServerReceiver(
+        address=receive_address,
+        reconnect=False,
+        timeout=5,
+        logger=logger
+    )
+    sender = ServerSender(
+        address=send_address,
+        to_send=receiver.received,
+        reconnect=False,
+        timeout=5,
+        logger=logger
+    )
+    watchdog = WatchDog(
+        {"receive": receiver.receive_thread, "send": sender.send_thread},
+        logger=logger
+    )
+
+    with receiver:
+        while sender:
+
+            receiver.start()
+            sender.start()
+            watchdog.start()
+
+            try:
+                watchdog.join()
+            except KeyboardInterrupt:
+                watchdog.shutdown()
+            finally:
+                receiver.shutdown()
+                sender.shutdown()
+
+    logger.info(f"Graceful shutdown")
+
+
+if __name__ == "__main__":
+    main()
